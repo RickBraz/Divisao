1c49aedc06b9428dcea5dcae26452a8a7ebaf10c
= <=>
2020-06-26 19:20:14 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Console.Write("(divisao) digite um numerador: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("agora digite o denominador: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z;
+            if ( y == 0)
+            {
+                Console.WriteLine("nao e possivel dividir por 0 ");
+            }
+            else 
+            {
+                 z = x / y;
+                Console.WriteLine($"{x} dividido por {y} é {z}");
+            }
+        }
+    }
+}
